b1aebdd9b0cfe39ee34959c541c49b87e3817eab
tutunak <dk@tutunak>
2021-04-02 18:49:10 +0300

    added demobucket

--- lib/aws-cdk-autoscaling-demo-stack.ts
+++ lib/aws-cdk-autoscaling-demo-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deployment from '@aws-cdk/aws-s3-deployment';
 
 export class AwsCdkAutoscalingDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const demoBucket = new s3.Bucket(this, "demoBucket", {
+      bucketName: "demobucket-98900110011",
+    })
+
+    new s3deployment.BucketDeployment(this, "demoBucketDeployment", {
+      destinationBucket: demoBucket, sources: [
+          s3deployment.Source.asset('../assets/simple-id-app/')
+      ]
+
+    })
   }
 }
